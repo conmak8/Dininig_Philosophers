af3d753f8fc2bb8677c0ef02b9da11f5ef831736
Constantinos Makariou <cmakario@student.42heilbronn.de>
2024-08-12 23:49:04 +0200

    found a workaround but i still take 200ms to finish

--- sources/philo_actions/supervisor.c
+++ sources/philo_actions/supervisor.c
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 00:13:30 by cmakario          #+#    #+#             */
-/*   Updated: 2024/08/12 22:42:16 by cmakario         ###   ########.fr       */
+/*   Updated: 2024/08/12 23:48:22 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	*monitor_philosophers(void *arg)
 {
 	int			i;
 	t_sim_data	*data ;
+	int 		total_meals;
 
 	data = (t_sim_data *)arg;
+	total_meals = 0;
 
 	usleep(5000);
 	while (true)
@@ -25,13 +27,16 @@ void	*monitor_philosophers(void *arg)
 		i = 0;
 		while (i < data->num_philosophers)
 		{
+			total_meals += data->philosophers[i].meals_count;
 			pthread_mutex_lock(&data->last_meal_mutex);
+			if ((data->required_meals * data->num_philosophers) >= total_meals) //---added this one
+				return(pthread_mutex_unlock(&data->last_meal_mutex), NULL);
 			if ((get_current_time() - data->philosophers[i].last_meal_time)
 				> data->death_time)
 			{
 				pthread_mutex_unlock(&data->last_meal_mutex);
-				log_status(data->philosophers, "died");
 				// printf("%lld %d %s\n", (get_current_time() - data->start_time), data->philosophers[i].id, "died");
+				log_status(data->philosophers, "died");
 				pthread_mutex_lock(&data->stop_mutex);
 				data->stop_simulation = 1;
 				pthread_mutex_unlock(&data->stop_mutex);
